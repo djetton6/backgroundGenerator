424cc5c24d877d98b223282e2bff675ad98415c4
Desmend Jetton <djetton@iit.edu>
2018-12-25 04:29:13 -0600

    3 new coding challenges with linear background.

--- app.js
+++ app.js
@@ -14,5 +14,16 @@ function setGradient() {
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+//
+body.addEventListener(onreadystatechange, function() {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+}
+)
 
-//input
+// //input
+// 1. Write code so that the colour inputs match the background generated on the first page load. 
+
+// 2. Display the initial CSS linear gradient property on page load.
+
+// 3. BONUS: Add a random button which generates two random numbers for the colour inputs.
